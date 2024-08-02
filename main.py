df00a1779cb407fbe6f3d8151798920f280e7329
Michael Brana <SerMike@users.noreply.github.com>
2024-08-02 16:15:58 -0400

    updated save functionality to save data into a json file instead of .txt. Will be implementing Search function

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
-# -------------------------------- PASSWORD GENERATOR ----------------------------------- #
+# -------------------------------------------- PASSWORD GENERATOR ---------------------------------------------------- #
 def generate_password():
     """Generates a strong password by randomly generating alphanumeric and special characters into a single output"""
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -26,37 +29,48 @@ def generate_password():
     # Join all character groups into one output and display it in 'Password' input field for user.
     strong_password = "".join(password_list)
     password_input.insert(0, strong_password)
-    pyperclip.copy(strong_password)     # Will save strong password to clipboard for easy pasting
+    pyperclip.copy(strong_password)  # Will save strong password to clipboard for easy pasting
 
-# -------------------------------- SAVE PASSWORD ----------------------------------- #
+
+# ------------------------------------------------ SAVE PASSWORD ----------------------------------------------------- #
 def save():
     """Saves inputs from 'Website', 'Email/Username', and 'Password' fields
-    into a data.txt file in a pipe delimited format. Clears contents from
+    into a data.json file in a pipe delimited format. Clears contents from
     'Website' and 'Password' fields"""
+
     web = website_input.get()
     em_un = email_un_input.get()
     pw = password_input.get()
+    new_data = {
+        web: {
+            "email": em_un,
+            "password": pw,
+        }
+    }
 
     # Checks to see if 'Website' or 'Password' input fields are empty
-    if len(web) == 0 or pw == 0:
-        messagebox.showwarning(title="Warning!", message="Please don't leave any fields blank!")
+    if len(web) == 0 or len(pw) == 0:
+        messagebox.showinfo(title="Warning!", message="Please don't leave any fields blank!")
     else:
-        is_ok = messagebox.askokcancel(title=web, message=f"These are the details entered: \n"
-                                                          f"Email: {em_un} \n"
-                                                          f"Password: {pw} \n"
-                                                          f"Is it ok to save?")
-        # If fields are populated prompt the user to 'Save' contents or cancel.
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{web} | {em_un} | {pw}\n")
-                website_input.delete(0, END)
-                password_input.delete(0, END)
-                website_input.focus()
-                messagebox.showinfo(title="Success!", message="Contents were successfully saved!\n"
-                                                              "Your strong password is copied to your clipboard.")
+        with open("data.json", "r") as data_file:
+            # Read existing data
+            data = json.load(data_file)
+            # Update existing data with new data
+            data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            # Save updated data
+            json.dump(data, data_file, indent=4)
+
+            # Clear existing user inputs from website and password fields
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+            website_input.focus()
+            # messagebox.showinfo(title="Success!", message="Contents were successfully saved!\n"
+            #                                              "Your strong password is copied to your clipboard.")
 
 
-# ------------------------------------- UI SETUP ---------------------------------------------- #
+# -------------------------------------------------- UI SETUP -------------------------------------------------------- #
 # Window Setup
 window = Tk()
 window.title("Password Manager")
@@ -67,7 +81,7 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-# Website Label & Entry
+# Website Label, Entry, & Search Button
 website = Label(text="Website:")
 website.grid(row=1, column=0)
 
@@ -96,6 +110,5 @@ pw_button.grid(row=3, column=2, sticky="nsew")
 # Add button
 add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="nsew")
-
 
 window.mainloop()
